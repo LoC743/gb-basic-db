c6b0e8e31bd7ad6bbf4060bfc8546a36fe4b26fd
Alexey Morozov <alexey1562@gmail.com>
2020-12-01 14:19:05 +0300

    Добавлено создание таблицы State

--- db-design/init_tables.sql
+++ db-design/init_tables.sql
@@ -8,3 +8,14 @@ CREATE TABLE `Country` (
   `subregion` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `State` (
+  `id` int NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `country_id` int NOT NULL,
+  `country_code` varchar(45) DEFAULT NULL,
+  `state_code` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_state_country_id_idx` (`country_id`),
+  CONSTRAINT `fk_state_country_id` FOREIGN KEY (`country_id`) REFERENCES `Country` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
