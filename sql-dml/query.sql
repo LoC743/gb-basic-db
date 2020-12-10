47e573cb75117c2823c4cf060977be77ddab99a3
Alexey Morozov <alexey1562@gmail.com>
2020-12-10 13:44:36 +0300

    Добавлен запрос (выбрать среднюю зарплату по отделам)

--- sql-dml/query.sql
+++ sql-dml/query.sql
@@ -40,3 +40,14 @@ WHERE
 
 -- База данных «Сотрудники»:
 -- 1. Выбрать среднюю зарплату по отделам.
+SELECT 
+    AVG(salary) average_salary,
+    dept_manager.dept_no,
+    departments.dept_name
+FROM
+    salaries
+        JOIN
+    dept_manager ON salaries.emp_no = dept_manager.emp_no
+        JOIN
+    departments ON departments.dept_no = dept_manager.dept_no
+GROUP BY dept_manager.dept_no
