ac20025f80c16870dda4262d882e143799e23df7
Alexey Morozov <alexey1562@gmail.com>
2020-12-10 13:15:24 +0300

    Добавлен запрос (выбрать все города из Московской области)

--- sql-dml/query.sql
+++ sql-dml/query.sql
@@ -14,3 +14,25 @@ FROM
     _countries ON _cities.country_id = _countries.id
         JOIN
     _regions ON _cities.region_id = _regions.id;
+
+
+-- 2. Выбрать все города из Московской области.
+SELECT 
+    _cities.id,
+    _cities.country_id,
+    _cities.important,
+    _cities.region_id,
+    _regions.title region_title,
+    _cities.title city_title
+FROM
+    _cities
+        JOIN
+    _regions ON _cities.region_id = _regions.id
+HAVING _regions.title = 'Московская область'
+--  Либо
+SELECT 
+    *
+FROM
+    _cities
+WHERE
+    region_id = 1053480
