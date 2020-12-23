e8fb3a29c5af507054f9a4bc58611b820fdd93a5
Alexey Morozov <alexey1562@gmail.com>
2020-12-23 14:47:20 +0300

    [7 Урок]: Добавлено третье задание собеседования

--- mysql-service/init_interview_tables.sql
+++ mysql-service/init_interview_tables.sql
@@ -51,3 +51,81 @@ CREATE TABLE `interview`.`matches` (
         REFERENCES `interview`.`_user` (`id`)
         ON DELETE NO ACTION
         ON UPDATE NO ACTION);
+
+
+-- Для задания 3
+CREATE TABLE `interview`.`photo` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `owner_id` INT NOT NULL,
+    `url` VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`id`),
+    INDEX `fk_owner_id_idx` (`owner_id` ASC) VISIBLE,
+    CONSTRAINT `fk_owner_id`
+        FOREIGN KEY (`owner_id`)
+        REFERENCES `interview`.`_user` (`id`)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION);
+
+CREATE TABLE `interview`.`comments` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `target_id` INT NOT NULL,
+    `user_id` INT NOT NULL,
+    `text` TEXT NOT NULL,
+    PRIMARY KEY (`id`),
+    INDEX `fk_target_id_idx` (`target_id` ASC) VISIBLE,
+    INDEX `fk_user_id_idx` (`user_id` ASC) VISIBLE,
+    CONSTRAINT `fk_comments_target_id`
+        FOREIGN KEY (`target_id`)
+        REFERENCES `interview`.`photo` (`id`)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION,
+    CONSTRAINT `fk_comments_user_id`
+        FOREIGN KEY (`user_id`)
+        REFERENCES `interview`.`_user` (`id`)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION);
+
+
+ALTER TABLE `interview`.`likes` 
+DROP FOREIGN KEY `fk_target_id`;
+ALTER TABLE `interview`.`likes` 
+ADD COLUMN `photo_target_id` INT NULL AFTER `target_id`,
+ADD COLUMN `comment_target_id` INT NULL AFTER `photo_target_id`,
+ADD COLUMN `type` VARCHAR(20) NOT NULL AFTER `comment_target_id`,
+CHANGE COLUMN `target_id` `target_id` INT NULL ,
+ADD INDEX `fk_likes_photo_target_id_idx` (`photo_target_id` ASC) VISIBLE,
+ADD INDEX `fk_likes_comments_target_id_idx` (`comment_target_id` ASC) VISIBLE;
+;
+ALTER TABLE `interview`.`likes` 
+ADD CONSTRAINT `fk_target_id`
+  FOREIGN KEY (`target_id`)
+  REFERENCES `interview`.`_user` (`id`),
+ADD CONSTRAINT `fk_likes_photo_target_id`
+  FOREIGN KEY (`photo_target_id`)
+  REFERENCES `interview`.`photo` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
+ADD CONSTRAINT `fk_likes_comment_target_id`
+  FOREIGN KEY (`comment_target_id`)
+  REFERENCES `interview`.`comments` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+
+DROP TRIGGER IF EXISTS `interview`.`likes_BEFORE_INSERT`;
+
+DELIMITER $$
+USE `interview`$$
+CREATE DEFINER=`root`@`localhost` TRIGGER `likes_BEFORE_INSERT` BEFORE INSERT ON `likes` FOR EACH ROW BEGIN
+IF EXISTS (
+    SELECT 1 FROM interview.likes
+    where (type = "photo" and target_id = NEW.photo_target_id and user_id = NEW.user_id)
+    OR (type = "comment" and target_id = NEW.comment_target_id and user_id = NEW.user_id)
+    OR (type = "user" and target_id = NEW.target_id and user_id = NEW.user_id)
+    ) THEN
+	SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = "Error: You can't like this item twice.";
+end if;
+END$$
+DELIMITER ;
+
+DELETE FROM likes WHERE user_id = 4 AND target_id = 5 AND type = "user";
+
